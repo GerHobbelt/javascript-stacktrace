ddc6b27a63f615cd07fa9293ff56e7267008b0fa
Ger Hobbelt <ger@hobbelt.com>
2019-06-14 22:41:58 +0200

    remove saucelabs deps

--- karma.conf.js
+++ karma.conf.js
@@ -24,7 +24,7 @@ module.exports = function(config) {
                 flags: ['--no-sandbox']
             }
         },
-        //browsers: ['ChromeCanary', 'Firefox'],
+        //browsers: ['PhantomJS2', 'Chrome', 'Firefox'],
         browsers: ['PhantomJS2'],
         singleRun: true
     });
