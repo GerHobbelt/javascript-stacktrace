49c7100749773f20028a7fcc9491701c0fa7e1f3
Ger Hobbelt <ger@hobbelt.com>
2019-06-14 22:00:07 +0200

    - tweak the TravisCI config - use `karma` directly, no `gulp` interference.

--- karma.conf.ci.js
+++ karma.conf.ci.js
@@ -131,6 +131,7 @@ module.exports = function(config) {
             'node_modules/stacktrace-gps/dist/stacktrace-gps.min.js',
             'node_modules/error-stack-parser/dist/error-stack-parser.js',
             'node_modules/stack-generator/dist/stack-generator.js',
+            'node_modules/jasmine-ajax/lib/mock-ajax.js',
             'stacktrace.js',
             'spec/fixtures/*.js',
             'spec/spec-helper.js',
